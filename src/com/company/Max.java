5a882d8aa589570bf3088fa9d701f03af9f07cb6
Gon-Zo <pnw123456@icloud.com>
2020-06-20 09:57:45 +0900

    #001 find Max Value

---
+++ src/com/company/Max.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Max {
+
+    /**
+     * Find Max Value
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("세 정수의 최대값을 구하여라 ");
+
+        System.out.println("a의 값은 ? :: ");
+        int a = scanner.nextInt();
+
+        System.out.println("b의 값은 ? :: ");
+        int b = scanner.nextInt();
+
+        System.out.println("c의 값은 ? :: ");
+        int c = scanner.nextInt();
+
+        int max = a;
+
+        if (b > max) {
+            max = b;
+        }
+
+        if (c > max) {
+            max = c;
+        }
+
+        System.out.println("Max Data is " + max);
+
+    }
+
+}
