a0d4e2a61979e9759dae415a57224ec61f43ac8a
gonzo <m165252@daum.net>
2020-07-19 19:21:04 +0900

    jpql 기본 예제

--- spring-jpa/src/main/java/com/example/springjpa/domain/post/support/PostSupport.java
+++ spring-jpa/src/main/java/com/example/springjpa/domain/post/support/PostSupport.java
@@ -1,4 +1,12 @@
 package com.example.springjpa.domain.post.support;
 
+import com.example.springjpa.domain.post.Post;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PostSupport {
+
+    Optional<List<Post>> findByAll();
+
 }
--- spring-jpa/src/main/java/com/example/springjpa/domain/post/support/PostSupportImpl.java
+++ spring-jpa/src/main/java/com/example/springjpa/domain/post/support/PostSupportImpl.java
@@ -1,14 +1,43 @@
 package com.example.springjpa.domain.post.support;
 
 import com.example.springjpa.domain.post.Post;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class PostSupportImpl extends QuerydslRepositorySupport implements PostSupport{
 
-    public PostSupportImpl() {
+    private final EntityManager entityManager;
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public PostSupportImpl(EntityManager entityManager, JPAQueryFactory jpaQueryFactory) {
         super(Post.class);
+        this.entityManager = entityManager;
+        this.jpaQueryFactory = jpaQueryFactory;
+    }
+
+    /**
+     * Jpql example method
+     *
+     * @return
+     */
+    @Override
+    public Optional<List<Post>> findByAll() {
+
+        String queryStr = "select * from post";
+
+        Query query = entityManager.createQuery(queryStr);
+
+        List<Post> result = query.getResultList();
+
+        return Optional.ofNullable(result);
     }
 
 }
