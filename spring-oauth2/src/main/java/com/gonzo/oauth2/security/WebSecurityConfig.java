fa315da93d6ca5756115466c5e4aafdb8a41ea22
gonzo <m165252@daum.net>
2020-08-16 19:11:46 +0900

    Spring OAuth2 Application Start

---
+++ spring-oauth2/src/main/java/com/gonzo/oauth2/security/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.gonzo.oauth2.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Create by park031517@gmail.com on 2020-08-16, 일
+ * Blog : https://zzz-oficial.tistory.com
+ * Github : https://github.com/Gon-Zo
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+    }
+
+}
--- spring-oauth2/src/main/java/com/gonzo/oauth2/web/HelloCotroller.java
+++ spring-oauth2/src/main/java/com/gonzo/oauth2/web/HelloCotroller.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Create by park031517@gmail.com on 2020-08-16, 일
+ * Blog : https://zzz-oficial.tistory.com
+ * Github : https://github.com/Gon-Zo
+ */
 @RestController
 @RequestMapping("/api")
-public class StaticsController {
+public class HelloCotroller {
 
     @GetMapping("/hello")
-    public String getHello(){
+    public String showHello(){
         return "Hello";
     }
 
