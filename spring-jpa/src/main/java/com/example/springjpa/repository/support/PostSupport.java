d9108d413f9afef4fd631c37ac24b988075e6034
gonzo <m165252@daum.net>
2021-01-06 21:58:21 +0900

    build: project
    - remove project

--- spring-jpa/src/main/java/com/example/springjpa/repository/support/PostSupport.java
+++
@@ -1,9 +0,0 @@
-package com.example.springjpa.repository.support;
-
-import java.util.List;
-
-public interface PostSupport {
-
-    List<String> findGroupByTitle();
-
-}
--- spring-jpa/src/test/java/com/example/springjpa/repository/CategoryRepositoryTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.springjpa.repository;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CategoryRepositoryTest {
-
-}
