bd5ea1c89a3be3446a71e6e7a056403f4b073d1b
gonzo <m165252@daum.net>
2021-05-10 20:18:55 +0900

    test: jasypt
    - jasypt test case

---
+++ jpa/src/test/java/io/gonzo/jpa/app/JasyptTest.java
@@ -0,0 +1,26 @@
+package io.gonzo.jpa.app;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
+public class JasyptTest {
+
+    public static void main(String[] args) {
+
+        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+
+        String encrypt = "";
+
+        jasypt.setPassword("");
+
+        jasypt.setAlgorithm("PBEWithMD5AndDES");
+
+        String encryptedText = jasypt.encrypt(encrypt);
+
+        String plainText = jasypt.decrypt(encryptedText);
+
+        System.out.println("encryptedText:  " + encryptedText);
+
+        System.out.println("plainText:  " + plainText);
+    }
+
+}
