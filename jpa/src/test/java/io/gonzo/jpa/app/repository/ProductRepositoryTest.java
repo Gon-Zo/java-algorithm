ec2e8fffd50477b84249e7dcff0f8470c110a39c
gonzo <m165252@daum.net>
2021-05-26 21:59:26 +0900

    test: [JPA Project] Dynamic Projections
    - A repository using a dynamic projection parameter

---
+++ jpa/src/test/java/io/gonzo/jpa/app/repository/ProductRepositoryTest.java
@@ -0,0 +1,12 @@
+package io.gonzo.jpa.app.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+
+    @Test
+    void findByImg() {
+    }
+}
