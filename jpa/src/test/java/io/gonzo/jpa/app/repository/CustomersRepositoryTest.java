a5ec49c8fe90a2172fbcbb60dc6330e7959ad3cb
gonzo <m165252@daum.net>
2021-06-04 23:43:56 +0900

    feat: [JPA Project] Query Creation
    - Query Creation Example
    - findBy

--- jpa/src/test/java/io/gonzo/jpa/app/repository/CustomersRepositoryTest.java
+++ jpa/src/test/java/io/gonzo/jpa/app/repository/CustomersRepositoryTest.java
@@ -13,8 +13,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @DataJpaTest
 @ActiveProfiles("h2")
 @ExtendWith(SpringExtension.class)
